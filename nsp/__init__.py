191032116ab018be23b7061b1fac16cce95de58d
felipaschwarz <felipaschwarz@gmail.com>
2021-10-08 00:11:16 +0300

    add outputLoader module to nsp

--- nsp/__init__.py
+++ nsp/__init__.py
@@ -2,3 +2,4 @@ from nsp.activations import Activations
 from nsp.nngraph import NNGraph
 from nsp.transformer import Transformer
 from nsp.visualizer import Visualizer
+from nsp.outputLoader import OutputLoader
