36ae7f72bb10ae45adf9b73c4299b8749d35ae05
felipaschwarz <felipaschwarz@gmail.com>
2021-10-09 13:11:48 +0300

    remove double torch

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.9'
+VERSION = '0.0.10'
 DESCRIPTION = 'Neural Network Signal Processing'
 LONG_DESCRIPTION = 'Analyze activation patterns of neural networks as causal signals on directed acyclic graphs (DAGs).'
 
@@ -20,6 +20,6 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=['numpy', 'scipy', 'torch', 'networkx', 'matplotlib', 'torch'],
+    install_requires=['numpy', 'scipy', 'torch', 'networkx', 'matplotlib'],
     keywords=['python', 'neural network', 'analysis', 'activations', 'activation pattern', 'fourier analysis'],
 )
